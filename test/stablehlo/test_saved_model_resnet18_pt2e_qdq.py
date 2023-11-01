327e6ce21a732420a480e8c6879633b1383f1528
Siyuan Liu <lsiyuan@google.com>
2023-11-01 22:16:52 +0000

    rename files
    refactor
    
    add quant util

--- test/stablehlo/test_saved_model_resnet18_pt2e_qdq.py
+++ test/stablehlo/test_saved_model_resnet18_pt2e_qdq.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torch.ao.quantization.quantize_pt2e import convert_pt2e, prepare_pt2e
 from torch.ao.quantization.quantizer.xnnpack_quantizer import (
@@ -7,23 +8,32 @@ from torch.ao.quantization.quantizer.xnnpack_quantizer import (
 from torch._export import capture_pre_autograd_graph
 import torchvision
 from torch_xla.experimental import quantize_utils
+import unittest
 
-# Step 1: export resnet18
-input_args = (torch.randn(1, 3, 224, 224),)
-m = torchvision.models.resnet18().eval()
-m = capture_pre_autograd_graph(m, input_args)
+os.environ['STABLEHLO_BYTECODE_FROM_PRETTYPRINT'] = '1'
 
-# Step 2: Insert observers or fake quantize modules
-quantizer = XNNPACKQuantizer().set_global(get_symmetric_quantization_config())
-m = prepare_pt2e(m, quantizer)
+class PT2EExportTest(unittest.TestCase):
+    def test_resnet18(self):
+        # Step 1: export resnet18
+        input_args = (torch.randn(1, 3, 224, 224),)
+        m = torchvision.models.resnet18().eval()
+        m = capture_pre_autograd_graph(m, input_args)
 
-# Step 3: Quantize the model
-m = convert_pt2e(m)
+        # Step 2: Insert observers or fake quantize modules
+        quantizer = XNNPACKQuantizer().set_global(get_symmetric_quantization_config())
+        m = prepare_pt2e(m, quantizer)
 
-# Trace with torch/xla and export stablehlo
-stablehlo_txt = quantize_utils.pt2e_reference_model_to_stablehlo(m, input_args)
-print(stablehlo_txt)
+        # Step 3: Quantize the model
+        m = convert_pt2e(m)
 
-# Save as tf.saved_model
-quantize_utils.pt2e_reference_model_to_tf_saved_model(
-    m, input_args, '/tmp/tf_saved_model/tmp1')
+        # Trace with torch/xla and export stablehlo
+        stablehlo_txt = quantize_utils.pt2e_reference_model_to_stablehlo(m, input_args)
+        print(stablehlo_txt)
+
+        # Save as tf.saved_model
+        quantize_utils.pt2e_reference_model_to_tf_saved_model(
+            m, input_args, '/tmp/tf_saved_model/tmp1')
+
+if __name__ == '__main__':
+  test = unittest.main()
+  sys.exit(0 if test.result.wasSuccessful() else 1)
